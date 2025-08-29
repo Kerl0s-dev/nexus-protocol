aaf6b067760b787fe53ba0cf69a4e4138cb0e01d
Kerl0s-dev <kezyah.larcher@outlook.fr>
2025-08-29 02:09:05 +0200

    Refactor AudioTrigger to use dynamic trigger array
    AudioTrigger now uses a public AITrigger[] array instead of hardcoded triggers. The scene and script were updated to support configurable trigger sequences for more flexible audio playback.

--- Assets/Scripts/AI/VoiceTrigger.cs
+++ Assets/Scripts/AI/VoiceTrigger.cs
@@ -4,12 +4,14 @@ public class AudioTrigger : MonoBehaviour
 {
     bool hasPlayed = false;
 
+    public AITrigger[] triggers;
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player")) // Vérifie que c'est bien le joueur
         {
             if (hasPlayed) return; // Empêche de rejouer si déjà joué
-            AIVoice.PlaySequentialAudio(AITrigger.Welcome, AITrigger.Rules, AITrigger.Advance);
+            AIVoice.PlaySequentialAudio(triggers);
             hasPlayed = true;
         }
     }
